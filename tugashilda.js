cc45fb57cc00a9f5cfbdbc49a7e5ce833382c39f
HildaDeliana <115686134+HildaDeliana@users.noreply.github.com>
2023-10-04 13:24:39 +0100

    Add files via upload

---
+++ tugashilda.js
@@ -0,0 +1,8 @@
+let firstName = "Hilda";
+let lastName = "Deliana";
+let age = 19;
+let isMarried = false;
+
+console.log(`Nama Lengkap : ${firstName} ${lastName}`);
+console.log(`Umur : ${age}`);
+console.log(`Is Married : ${isMarried}`);
