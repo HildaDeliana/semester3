448ff757ed1381d76bf70e8fba9106aa33057fe0
HildaDeliana <115686134+HildaDeliana@users.noreply.github.com>
2023-10-20 16:03:09 +0100

    Add files via upload

---
+++ No1.js
@@ -0,0 +1,7 @@
+const restaurant = {
+  name: "Nanami",
+  city: "jakarta",
+  favoriteDrink: "jus apocado",
+  favoriteFood: "spagethi",
+  isVegan: true // 
+};
---
+++ No2.js
@@ -0,0 +1,16 @@
+const restoran = [
+  {
+    name: "PHD",
+    city: "jakarta",
+    minumanFavorit: "juice lemon",
+    makananFavorit: "pizza",
+    isVegan: true
+  },
+  {
+    name: "MCD",
+    city: "Sukabumi",
+    minumanFavorit: "cola",
+    makananFavorit: "fried chicken",
+    isVegan: false
+  }
+  ];
